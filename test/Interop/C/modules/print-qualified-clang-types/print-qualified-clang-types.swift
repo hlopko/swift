77de0b9857ba765ccc2d56aca4d34d70230b4071
Martin Boehme <mboehme@google.com>
2020-06-19 12:49:18 +0200

    Add explanatory comment to test.

--- test/Interop/C/modules/print-qualified-clang-types/print-qualified-clang-types.swift
+++ test/Interop/C/modules/print-qualified-clang-types/print-qualified-clang-types.swift
@@ -1,3 +1,11 @@
+// Check that when qualifying Clang types with a module name, we choose a
+// visible module. Clang types need special treatment because multiple Clang
+// modules can contain the same type declarations from a textually included
+// header, but not all of these modules may be visible. If we choose a module
+// that isn't visible, we produce `.swiftinterface` files that don't compile.
+//
+// This is a regression test for https://bugs.swift.org/browse/SR-13032.
+
 // RUN: %empty-directory(%t)
 // RUN: mkdir %t/sub_module %t/main_module
 // RUN: %target-swift-frontend -enable-library-evolution -swift-version 5 -emit-module -o %t/sub_module/SubModule.swiftmodule %S/Inputs/SubModule.swift -I %S/Inputs
